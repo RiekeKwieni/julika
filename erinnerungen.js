25503f4d001e6d8db270bcf551e30ad0409f38b1
Rieke Matzke <rieke.matzke@uni-muenster.de>
2026-08-07 23:25:35 +0200

    neue erinnerungen ergänzt

--- erinnerungen.js
+++ erinnerungen.js
@@ -19,4 +19,38 @@ omg weisst du noch damals ich ich in der mensa nur vegetarisch gegessen hab weil
 weisst du noch damals als ich 1 woche in amerika war ... schlimmste woche EVER
 weisst du noch damals als wir blumengarten durch einen zaun gesehen haben auf dem domplatz es war so toll
 OMG weisst du noch damals als du HARRY FUCKING STYLES warst. naja nicht so cool wie Julika FUCKING IDA HENKE
+Weißt du noch damals als wir Linsensuppe im Ex-Cafe aßen
+Weißt du noch damals als wir frisch in Münster waren und ewig lang in einem Hauseingang auf dem Hansaring yappten
+Weißt du noch damals als wir in deiner alten WG mit Rieke und T*m Tabu spielten und starben, weil T*m keine Sprichwörter kennt
+Weißt du noch damals als der nackte Mann im Möhnesee an uns vorbeistolzierte
+Weißt du noch damals als wir beim Zsmschrauben deiner DHL-Postbox beinahe crash out gingen
+Weißt du noch damals als wir bei Aaron „21 wird Elle Kutter“ aufnahmen
+Weißt du noch damals als wir das erste feini weini Kochen in Elles Küche veranstalteten
+Weißt du noch damals als wir den ersten Viertel-Post machten und das iconic Boomernag-Video von Rieke entstand, auf dem sie in Windeseile die Straße überquert
+Weißt du noch damals als wir mit Nele in den Rieselfeldern slayten 
+Weißt du noch damals als wir mir 200 Rentnern und einem Reseller um 20 Uhr für Edelfundus anstanden
+Weißt du noch damals als wir wandern waren und Rieke nur knapp überlebte 
+Weißt du noch damals als wir zusammen mit Gang Princess Charming schauten
+Weißt du noch damals als du Sylvester durch Espresso Martini beinah auf einen Drogentrip befördert wurdest
+Weißt du noch damals als Julian immer überall jemals auftauchte (hoffentlich nicht in Bergen !?) 
+Weißt du noch damals als wir das leere Zimmer deiner alten WG als Singzimmer nutzten
+Weißt du noch damals als wir das gesamte Obst des FH-Gartens plünderten
+Weißt du noch damals als wir Penglord b2b P.vanillaboy im Amp sahen
+Weißt du noch damals als wir zu One Direction mutierten, um Riekes Bday zu zelebrieren
+Weißt du noch damals als wir versehentlich extrem gut matchten in unseren bunte Strumpfhosen rot blau vor dem Bauschuttcontainer gegenüber vom Supernormal Outfits
+Weißt du noch damals als wir Aaron zum Bday einen Poetry Slam verfassten
+Weißt du noch damals als deine Einkaufstüte wortwörtlich in die Brüche ging und ich zur Rettung eilen musste
+Weißt du noch damals als wir mit Helen Blumensträußchen für die WeeGee pflückten
+Weißt du noch damals als wir mit Dünnbert schillten
+Weißt du noch damals als du mir in deiner alten WG Küche von BITTE BITTE ICH SCHAUE NICHT IN MEINE BAUCHTASCHE erzähltest
+Weißt du noch damals als wir für einen kostenlosen Platz auf dem Promenaden-Flohmarkt um 5 Uhr aufstanden und um unser Leben kämpften.. komme promme wir flohmen !??!
+Weißt du noch damals als wir Souly live sahen? am Heaven Beach!?
+Weißt du noch als du mich mit meinem neuen Namen gesegnet hast? #charlykforever
+Weißt du noch als wir realisiert haben dass wir erst zur NATO müssen um Volo bei der Zeit zu machen? Nummer eins Feind
+Weißt du noch als wir gefühlt eine ganze Bahnfahrt über Bücher geredet haben und du mir dann eins davon zum Geburtstag geschenkt hast? Das war extrem mausig von dir 
+Weißt du noch als wir unbedingt ins Kino wollten und die Auswahl so schlecht war dass wir in Jurassic World (Park?? kp) gelandet sind? Gar keine Ahnung mehr was der Plot war aber war lustig
+lol weißt du noch damals als wir zsm in der geo bib saßen und dann KOMPLETT synchron raus geschaut und dann regenradar geöffnet haben. denke tatsächlich jedes mal daran wenn ich nach regen schaue am laptop lol
+weißt du noch damals als wir zsm mit ehmi und ihr bf im freibad waren und du am nächsten tag gesagt hast dass du noch nie meine füße gesehen hast …
+weißt du noch damals als wir zsm im hallenbad kinderhaus waren und uns die extreme kwien von dem hochwasser damals berichtete
+weißt du noch damals als wir in neheim hayat durch die fußgängerzone gelaufen sind und eis gegessen haben
 `;
